d7bd51882cd056e7860b8627d029f793214a165d
pedrosilva <pedro.pgs1998@gmail.com>
2022-11-30 21:39:08 -0300

    Add DataBaseConfiguration Dev e Local

---
+++ src/main/java/seapa/back/Settings/Configuration/DataBaseLocalConfiguration.java
@@ -0,0 +1,40 @@
+package seapa.back.Settings.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("local")
+public class DataBaseLocalConfiguration {
+
+    @Bean
+    public DataSource dataSourceLocal() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+
+        adapter.setDatabase(Database.H2);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+        adapter.setPrepareConnection(true);
+
+        return adapter;
+    }
+}
