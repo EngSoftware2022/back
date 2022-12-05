d618ac8d4e2c050500b23c7b654738db7f94763b
pedrosilva <pedro.pgs1998@gmail.com>
2022-12-05 08:52:49 -0300

    fix cors WebMvc

---
+++ src/main/java/seapa/back/Settings/Configuration/WebMvcConfiguration.java
@@ -0,0 +1,25 @@
+package seapa.back.Settings.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+                .allowCredentials(true);
+    }
+
+}
