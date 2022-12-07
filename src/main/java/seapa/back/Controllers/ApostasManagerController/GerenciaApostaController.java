415b0a99cdb9989d473359f5ee97ebc517a6d84b
Marcos Villar <marcosvillar96@gmail.com>
2022-12-07 12:39:57 -0300

    Update GerenciaApostaController.java

--- src/main/java/seapa/back/Controllers/ApostasManagerController/GerenciaApostaController.java
+++ src/main/java/seapa/back/Controllers/ApostasManagerController/GerenciaApostaController.java
@@ -3,8 +3,8 @@ package seapa.back.Controllers.ApostasManagerController;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 import seapa.back.Entitys.UserManegerEntitys.ApostasEntitys.GerenciadorApostas;
 import seapa.back.Entitys.UserManegerEntitys.GrupoEntitys.Grupo;
 import seapa.back.Models.CriarGerenciaAposta;
@@ -25,49 +25,44 @@ public class GerenciaApostaController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void insertGerenciadorAposta(@RequestBody CriarGerenciaAposta gerenciadorApostas) {
-        try {
-            Grupo grupo = timeRepository.findById(gerenciadorApostas.getGrupoId()).isPresent() ? timeRepository.findById(gerenciadorApostas.getGrupoId()).get() : null;
+    public ResponseEntity<Object> insertGerenciadorAposta(@RequestBody CriarGerenciaAposta gerenciadorApostas) {
+        Grupo grupo = timeRepository.findById(gerenciadorApostas.getGrupoId()).isPresent() ? timeRepository.findById(gerenciadorApostas.getGrupoId()).get() : null;
 
-            if (grupo == null) {
-                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
-            }
-
-            GerenciadorApostas gerenciaAposta = gerenciadorApostas.conversor(grupo);
-            gerenciadorApostasRepository.save(gerenciaAposta);
-        } catch (Exception e) {
-            throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
+        if (grupo == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+
+        GerenciadorApostas gerenciaAposta = gerenciadorApostas.conversor(grupo);
+        gerenciadorApostasRepository.save(gerenciaAposta);
+        return null;
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping(value = "/editarGerenciaAposta")
-    public void editarGerenciadorAposta(@RequestBody EditarGerenciaAposta editarGerenciaAposta) {
-        try {
-            if (Objects.equals(editarGerenciaAposta.getDataInicio(), new Date())) {
-                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
-            }
-
-            Grupo grupo = timeRepository.findById(editarGerenciaAposta.getGrupoId()).isPresent() ? timeRepository.findById(editarGerenciaAposta.getGrupoId()).get() : null;
-
-            if (grupo == null) {
-                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
-            }
-            if (Objects.equals(grupo.getModeradorId(), editarGerenciaAposta.getIdUsuario())) {
-                GerenciadorApostas gerenciaApostaExistente = gerenciadorApostasRepository.findById(editarGerenciaAposta.getIdGerenciaAposta()).isPresent() ? gerenciadorApostasRepository.findById(editarGerenciaAposta.getIdGerenciaAposta()).get() : null;
-
-                if (gerenciaApostaExistente == null) {
-                    throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
-                }
-                GerenciadorApostas gerenciaApostaAtualizado = editarGerenciaAposta.conversor(grupo);
-                gerenciaApostaAtualizado.setId(gerenciaApostaExistente.getId());
-
-                gerenciadorApostasRepository.save(gerenciaApostaAtualizado);
-            } else {
-                throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
-            }
-        } catch (Exception e) {
-            throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Object> editarGerenciadorAposta(@RequestBody EditarGerenciaAposta editarGerenciaAposta) {
+        if (Objects.equals(editarGerenciaAposta.getDataInicio(), new Date())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+
+        Grupo grupo = timeRepository.findById(editarGerenciaAposta.getGrupoId()).isPresent() ? timeRepository.findById(editarGerenciaAposta.getGrupoId()).get() : null;
+
+        if (grupo == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        if (Objects.equals(grupo.getModeradorId(), editarGerenciaAposta.getIdUsuario())) {
+            GerenciadorApostas gerenciaApostaExistente = gerenciadorApostasRepository.findById(editarGerenciaAposta.getIdGerenciaAposta()).isPresent() ? gerenciadorApostasRepository.findById(editarGerenciaAposta.getIdGerenciaAposta()).get() : null;
+
+            if (gerenciaApostaExistente == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+            GerenciadorApostas gerenciaApostaAtualizado = editarGerenciaAposta.conversor(grupo);
+            gerenciaApostaAtualizado.setId(gerenciaApostaExistente.getId());
+
+            gerenciadorApostasRepository.save(gerenciaApostaAtualizado);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return null;
     }
 }
+
