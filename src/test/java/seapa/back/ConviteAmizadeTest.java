fa783ce9037263d062831dfcfea6b10d09ef355d
Magno_Melo <magno.melo@maps.com.br>
2022-12-07 15:47:54 -0300

    convite amizade test e usuariocontroller test

--- src/test/java/seapa/back/ConviteAmizadeTest.java
+++ src/test/java/seapa/back/ConviteAmizadeTest.java
@@ -1,12 +1,23 @@
 package seapa.back;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import seapa.back.Controllers.UserManagerController.ConviteAmizadeController;
 import seapa.back.Entitys.UserManegerEntitys.UserEntitys.ContaUsuario;
+import seapa.back.Entitys.UserManegerEntitys.UserEntitys.ConviteAmizade;
+import seapa.back.Models.ConviteAmizadeDTO;
 import seapa.back.Repository.UserManagerRepository.ContaUsuarioRepository;
 import seapa.back.Repository.UserManagerRepository.ConviteAmizadeRepository;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class ConviteAmizadeTest {
@@ -22,6 +33,7 @@ public class ConviteAmizadeTest {
 
     ContaUsuario contaUsuario1 = new ContaUsuario();
     ContaUsuario contaUsuario2 = new ContaUsuario();
+    ConviteAmizade conviteAmizade = new ConviteAmizade();
 
     @BeforeEach
     public void beforeEach(){
@@ -37,7 +49,42 @@ public class ConviteAmizadeTest {
         contaUsuario2.setStatusConta("maria");
         contaUsuarioRepository.save(contaUsuario1);
         contaUsuarioRepository.save(contaUsuario2);
+        conviteAmizade.setSolicitado(contaUsuario2);
+        conviteAmizade.setSolicitante(contaUsuario1);
+        conviteAmizade.setId(1L);
+        conviteAmizade.setStatusConvite("PENDENTE");
+        repository.save(conviteAmizade);
     }
+
+    @Test
+    public void conferindoSeConviteFoiEnviado(){
+        ResponseEntity<List<ConviteAmizadeDTO>> REConvite =  controller.findAllConvitesPendentesByUsuarioId(1L);
+        ConviteAmizadeDTO convite = REConvite.getBody().get(0);
+        assertTrue(Objects.nonNull(convite));
+        assertEquals(convite.getSolicitadoId(), 2L);
+        assertEquals(convite.getSolicitanteId(), 1L);
+    }
+
+    @Test
+    public void recusandoConviteDeAmizade(){
+        ResponseEntity response = controller.atualizaStatusConviteAmizade(1L, "REJEITADO");
+        String resposta = response.getBody().toString();
+        assertEquals(resposta , "Convite de amizade rejeitado com SUCESSO");
+    }
+
+    @Test
+    public void aceitandoConviteDeAmizade(){
+        ResponseEntity response = controller.atualizaStatusConviteAmizade(1L, "ACEITO");
+        String resposta = response.getBody().toString();
+        assertEquals(resposta , "Convite de amizade aceito com SUCESSO");
+    }
+
+    @Test
+    public void deletandoConviteDeAmizade(){
+        ResponseEntity response = controller.deleteConviteAmizade(1L);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
 
 
 
