c748b05b5a99363e40a9aaaabfaa0a6d7f5fe04a
Matheus Lima <lima.matheus@unifesp.br>
2022-12-20 19:12:13 -0300

    Validar vencedores da aposta; Mudanças nas entidades de apostas;

--- src/main/java/seapa/back/Entitys/UserManegerEntitys/ApostasEntitys/GerenciadorApostas.java
+++ src/main/java/seapa/back/Entitys/UserManegerEntitys/ApostasEntitys/GerenciadorApostas.java
@@ -33,6 +33,16 @@ public class GerenciadorApostas extends Auditable<String> implements Serializabl
     @Column(name = "tipo_esporte")
     private String tipoEsporte;
 
+    /*
+    @JoinColumn(name = "equipe_id")
+    @Column(name = "equipe_alpha_id")
+    private Equipe equipeAlpha;
+
+    @JoinColumn(name = "equipe_id")
+    @Column(name = "equipe_beta_id")
+    private Equipe equipeBeta;
+     */
+
     @Column(name = "tipo_gerenciamento")
     private String tipoGerenciamento;
 
