13054f205ffc42a56cd89db064101a69ecc4453d
agoulart <agoulart@outlook.com>
2023-01-14 04:06:15 -0300

    change generateValue

--- src/main/java/seapa/back/Entitys/UserManegerEntitys/GrupoEntitys/IntegrantesGrupo.java
+++ src/main/java/seapa/back/Entitys/UserManegerEntitys/GrupoEntitys/IntegrantesGrupo.java
@@ -6,12 +6,11 @@ import javax.persistence.*;
 
 @Data
 @Entity
-@SequenceGenerator(name = "integrantes_seq", sequenceName = "integrantes_seq", allocationSize = 1, initialValue = 1)
 @Table(name = "SEAPA_INTEGRANTES_GRUPO")
 public class IntegrantesGrupo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "integrantes_seq")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
--- src/main/java/seapa/back/Repository/TeamManagerRepository/IntegranteGrupoRepository.java
+++
@@ -1,9 +0,0 @@
-package seapa.back.Repository.TeamManagerRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import seapa.back.Entitys.UserManegerEntitys.GrupoEntitys.IntegranteGrupo;
-
-@Repository
-public interface IntegranteGrupoRepository extends JpaRepository<IntegranteGrupo, Long> {
-}
